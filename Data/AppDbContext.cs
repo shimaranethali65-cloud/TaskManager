2197963257665f052e79833e5f5ddc41f9a5bd71
shimaranethali65-cloud <shimaranethali65@gmail,com>
2026-04-18 02:10:56 +0530

    Implemented the sign up page and database Integration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApp.Models; // adjust if needed
+
+namespace TaskManagerApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
